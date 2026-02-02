35f91a89d20276d3dc1bfa1140f22669fcc4db83
Amine Nouri <nouri.webdev@gmail.com>
2026-02-02 22:48:48 +0100

    add font

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
   <!-- Charm & Fuzzy Bubbles -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Fuzzy+Bubbles:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Grandstander:wght@100..900&display=swap" rel="stylesheet">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet" />
